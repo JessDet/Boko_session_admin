7c22ee2ea44b7117ba7ac908df4de825ba62e07c
Jess.D <jessica.detrue@gmail.com>
2022-01-09 17:19:03 +0100

    MAJ recettes

--- Admin_loggout.php
+++ Admin_loggout.php
@@ -1,8 +1,14 @@
 <?php
 
-session_start();
-$_SESSION = array();
-session_destroy();
-header('Location: /Admin_loggin.php');
+$pdo = require_once './database.php';
+$sessionadminId = $_COOKIE['sessionadmin'] ?? '';
 
-?>
+if ($sessionadminId) {
+    $statement = $pdo->prepare('DELETE FROM sessionadmin WHERE id=:id');
+    $statement->bindValue(':id', $sessionadminId);
+    $statement->execute();
+    setcookie('sessionadmin', '', time() -1);
+    header('Location: /index.php');
+}else {
+    header('Location: /index.php');
+}
